b14b841bd5f920fb62686fd7e63f558fd8684d8d
AnandRaj234 <106963892+AnandRaj234@users.noreply.github.com>
2023-01-25 02:29:32 +0530

    s

--- src/test/java/com/javatechie/azure/SpringbootAzuresqlApplicationTests.java
+++ src/test/java/com/javatechie/azure/SpringbootAzuresqlApplicationTests.java
@@ -1,10 +1,10 @@
-package life.care.health;
+package com.javatechie.azure;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class HealthApplicationTests {
+class SpringbootAzuresqlApplicationTests {
 
 	@Test
 	void contextLoads() {
