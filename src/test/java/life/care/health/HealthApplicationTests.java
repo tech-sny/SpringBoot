4127ce06001f307c7b98354aee3f68cc3f58c3e2
AnandRaj234 <106963892+AnandRaj234@users.noreply.github.com>
2023-01-25 02:06:35 +0530

    .

---
+++ src/test/java/life/care/health/HealthApplicationTests.java
@@ -0,0 +1,13 @@
+package life.care.health;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
