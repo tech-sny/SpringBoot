6d32dab0cf01f9a610dff073a09299801bba2180
AnandRaj234 <106963892+AnandRaj234@users.noreply.github.com>
2023-01-29 15:01:42 +0530

    Update SpringbootAzuresqlApplication.java

--- src/main/java/life/care/health/azure/SpringbootAzuresqlApplication.java
+++ src/main/java/life/care/health/azure/SpringbootAzuresqlApplication.java
@@ -1,14 +1,18 @@
 package life.care.health.azure;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import life.care.health.fileUpload.FileService;
+import life.care.health.azure.Employee;
 
+import java.io.IOException;
 import java.util.List;
 
 
@@ -19,9 +23,18 @@ public class SpringbootAzuresqlApplication {
     @Autowired
     private EmployeeRepository repository;
 
-    @PostMapping
-    public Employee addEmployee(@RequestBody Employee employee) {
-        return repository.save(employee);
+    @Autowired
+    FileService fileService;
+    @Autowired
+    private ObjectMapper mapper;
+
+    
+	@PostMapping(consumes = {MediaType.APPLICATION_JSON_VALUE,MediaType.MULTIPART_FORM_DATA_VALUE})
+    public Employee createCondition(@RequestPart("data") String employee,@RequestPart("image") MultipartFile image) throws IOException{
+        String img = fileService.upload(image, "prefix");
+        Employee e = mapper.readValue(employee,Employee.class);
+        e.setImage(img);
+        return repository.save(e);
     }
 
     @GetMapping
